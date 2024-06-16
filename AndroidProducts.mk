555de95bfaaf7cff5200958e709d49e677334adf
redeem-freefire <rabikishanr@gmail.com>
2024-06-16 19:49:10 +0545

    X657B: Init TWRP TREE

---
+++ AndroidProducts.mk
@@ -0,0 +1,12 @@
+#
+# Copyright (C) 2024 The Android Open Source Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/twrp_x657b.mk
+
+COMMON_LUNCH_CHOICES := \
+    twrp_x657b-user \
+    twrp_x657b-userdebug \
+    twrp_x657b-eng
---
+++ device.mk
@@ -0,0 +1,6 @@
+#
+# Copyright (C) 2024 The Android Open Source Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+LOCAL_PATH := device/infinix/x657b
