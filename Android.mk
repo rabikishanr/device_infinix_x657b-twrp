701cf5d30cfe7400ef6ef2f728f91d2e6583cea4
Rabikishan Rauniyar <154669639+rabikishanr@users.noreply.github.com>
2024-06-16 21:09:32 +0545

    X657B: some fixes

--- Android.mk
+++ Android.mk
@@ -1,12 +1,10 @@
 #
 # Copyright (C) 2024 The Android Open Source Project
-# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
-#
 # SPDX-License-Identifier: Apache-2.0
 #
 
 LOCAL_PATH := $(call my-dir)
 
-ifeq ($(TARGET_DEVICE),Infinix-X657B)
+ifeq ($(TARGET_DEVICE),x657b)
 include $(call all-subdir-makefiles,$(LOCAL_PATH))
 endif
