0382cbbfca6c0f380959a794ef068262b5ea0018
Rabikishan Rauniyar <154669639+rabikishanr@users.noreply.github.com>
2024-06-17 11:35:23 +0545

    X657B: fix for compilation

--- twrp_x657b.mk
+++ twrp_x657b.mk
@@ -1,26 +1,19 @@
-#
-# Copyright (C) 2024 The Android Open Source Project
-# SPDX-License-Identifier: Apache-2.0
-#
-
 # Inherit from those products. Most specific first.
+$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-
-# Inherit some common twrp stuff.
-$(call inherit-product, vendor/twrp/config/common.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
 # Inherit from x657b device
 $(call inherit-product, device/infinix/x657b/device.mk)
 
+# Inherit some common twrp stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
+$(call inherit-product, vendor/twrp/config/gsm.mk)
+
+# Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := x657b
 PRODUCT_NAME := twrp_x657b
 PRODUCT_BRAND := Infinix
-PRODUCT_MODEL := Infinix X657B
+PRODUCT_MODEL := Infinix x657b
 PRODUCT_MANUFACTURER := infinix
-
-PRODUCT_GMS_CLIENTID_BASE := android-transsion-infinix-rev1
-
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="full_x657b_h6117-user 10 QP1A.190711.020 63466 release-keys"
-
-BUILD_FINGERPRINT := Infinix/X657B-OP/x657b:10/QP1A.190711.020/DFJ-OP-220822V438:user/release-keys
+PRODUCT_RELEASE_NAME := Infinix x657b
