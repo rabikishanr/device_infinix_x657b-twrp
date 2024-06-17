402bcbe08f86676d3de05bd81b947e2cfc7e48c0
Rabikishan Rauniyar <154669639+rabikishanr@users.noreply.github.com>
2024-06-17 12:33:37 +0545

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,6 +5,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-add_lunch_combo omni_Infinix-X657B-user
-add_lunch_combo omni_Infinix-X657B-userdebug
-add_lunch_combo omni_Infinix-X657B-eng
+add_lunch_combo omni_x657b-user
+add_lunch_combo omni_x657b-userdebug
+add_lunch_combo omni_x657b-eng
